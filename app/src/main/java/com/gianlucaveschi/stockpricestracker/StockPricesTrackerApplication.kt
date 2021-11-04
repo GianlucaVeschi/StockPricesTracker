ba9ffb330acbb485b8c160ea0fa1d2cc7c21fd19
g.veschi <g.veschi@thalia.de>
2021-11-04 10:04:28 +0100

    Initial commit

---
+++ app/src/main/java/com/gianlucaveschi/stockpricestracker/StockPricesTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.gianlucaveschi.stockpricestracker
+
+import android.app.Application
+
+class StockPricesTrackerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
