d13540b219964e860cf957b6c6522f3766e0e592
g.veschi <g.veschi@thalia.de>
2021-11-05 11:23:51 +0100

    Add parameter to UseCases

--- app/src/main/java/com/gianlucaveschi/stockpricestracker/interactors/StartSubscriptionToStockMarketUseCase.kt
+++ app/src/main/java/com/gianlucaveschi/stockpricestracker/interactors/StartSubscriptionToStockMarketUseCase.kt
@@ -2,14 +2,11 @@ package com.gianlucaveschi.stockpricestracker.interactors
 
 import com.gianlucaveschi.stockpricestracker.domain.model.TickerInfo
 import com.gianlucaveschi.stockpricestracker.repo.MainRepository
-import timber.log.Timber
 
 class StartSubscriptionToStockMarketUseCase(
     private val mainRepository: MainRepository
 ) {
-    fun run() {
-        Timber.d("start observation")
-        val tickerInfo = TickerInfo("Apple","US0378331005")
+    fun run(tickerInfo: TickerInfo) {
         mainRepository.subscribeToTicker(tickerInfo)
     }
 }
--- app/src/main/java/com/gianlucaveschi/stockpricestracker/interactors/StopSubscriptionToStockMarketUseCase.kt
+++ app/src/main/java/com/gianlucaveschi/stockpricestracker/interactors/StopSubscriptionToStockMarketUseCase.kt
@@ -2,14 +2,11 @@ package com.gianlucaveschi.stockpricestracker.interactors
 
 import com.gianlucaveschi.stockpricestracker.domain.model.TickerInfo
 import com.gianlucaveschi.stockpricestracker.repo.MainRepository
-import timber.log.Timber
 
 class StopSubscriptionToStockMarketUseCase(
     private val mainRepository: MainRepository
 ) {
-    fun run() {
-        Timber.d("stop observation")
-        val tickerInfo = TickerInfo("Apple","US0378331005")
+    fun run(tickerInfo: TickerInfo) {
         mainRepository.unsubscribeFromTicker(tickerInfo)
     }
 }
