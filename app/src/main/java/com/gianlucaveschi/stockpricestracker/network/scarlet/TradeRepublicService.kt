b7c571dd5d67c38e3da5f32e5bc6d0a1915df0d3
g.veschi <g.veschi@thalia.de>
2021-11-05 15:15:55 +0100

    Fix service

--- app/src/main/java/com/gianlucaveschi/stockpricestracker/network/scarlet/TradeRepublicService.kt
+++ app/src/main/java/com/gianlucaveschi/stockpricestracker/network/scarlet/TradeRepublicService.kt
@@ -1,18 +1,31 @@
 package com.gianlucaveschi.stockpricestracker.network.scarlet
 
+import com.gianlucaveschi.stockpricestracker.domain.model.ScarletTickerApiModel
+import com.gianlucaveschi.stockpricestracker.domain.model.ScarletTickerSubscription
+import com.gianlucaveschi.stockpricestracker.domain.model.TickerApiModel
+import com.gianlucaveschi.stockpricestracker.domain.model.TickerSubscription
 import com.tinder.scarlet.WebSocket
 import com.tinder.scarlet.ws.Receive
 import com.tinder.scarlet.ws.Send
+import io.reactivex.Flowable
 import kotlinx.coroutines.flow.Flow
 
 interface TradeRepublicService {
-
-    @Receive
-    fun observeWebSocket(): Flow<WebSocket.Event>
-
+    /**
+     * A @Send annotated method takes one parameter.
+     * When invoked, it sends an outgoing message to the server.
+     * */
     @Send
-    fun sendSubscribe(subscription: String)
+    fun sendSubscribe(subscribe: ScarletTickerSubscription)
+
+    /**
+     * A @Receive annotated method returns a stream of incoming messages or events about the current connection state.
+     * You need to subscribe to the stream to observe values.
+     * */
+    @Receive
+    fun observeTicker(): Flow<ScarletTickerApiModel>
 
     @Receive
-    fun observeTicker(): Flow<String>
+    fun observeOnConnectionOpenedEvent(): Flow<WebSocket.Event>
+
 }
