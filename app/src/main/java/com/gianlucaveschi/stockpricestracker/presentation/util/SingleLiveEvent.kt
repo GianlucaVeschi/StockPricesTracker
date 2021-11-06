cc264f301acee8a0423b5b368f2c872c62dc92f0
g.veschi <g.veschi@thalia.de>
2021-11-06 17:22:36 +0100

    Add credits

--- app/src/main/java/com/gianlucaveschi/stockpricestracker/presentation/util/SingleLiveEvent.kt
+++ app/src/main/java/com/gianlucaveschi/stockpricestracker/presentation/util/SingleLiveEvent.kt
@@ -11,13 +11,14 @@ import java.util.concurrent.atomic.AtomicBoolean
  * A lifecycle-aware observable that sends only new updates after subscription, used for events like
  * navigation and Snackbar messages.
  *
- *
  * This avoids a common problem with events: on configuration change (like rotation) an update
  * can be emitted if the observer is active. This LiveData only calls the observable if there's an
  * explicit call to setValue() or call().
  *
- *
  * Note that only one observer is going to be notified of changes.
+ *
+ * Credits :
+ * https://github.com/android/architecture-samples/blob/dev-todo-mvvm-live/todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/SingleLiveEvent.java
  */
 class SingleLiveEvent<T> : MutableLiveData<T>() {
     private val pending = AtomicBoolean(false)
@@ -27,7 +28,6 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
         if (hasActiveObservers()) {
             Timber.w("Multiple observers registered but only one will be notified of changes.")
         }
-        // Observe the internal MutableLiveData
         super.observe(owner, Observer { t ->
             if (pending.compareAndSet(true, false)) {
                 observer.onChanged(t)
@@ -39,13 +39,5 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
     override fun setValue(t: T?) {
         pending.set(true)
         super.setValue(t)
-    }
-
-    /**
-     * Used for cases where T is Void, to make calls cleaner.
-     */
-    @MainThread
-    fun call() {
-        value = null
     }
 }
